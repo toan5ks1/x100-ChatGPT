6d4d5aaa134bd816d0f250e0349e2179cb0a28fc
toan5ks1 <vipteamvnclass@gmail.com>
2024-10-11 00:46:25 +0700

    feat: active share dont right

--- packages/shared/lib/utils/constant.ts
+++ packages/shared/lib/utils/constant.ts
@@ -1,3 +1,5 @@
 export const hostUrl = 'https://chatgpt.com';
 export const conversationUrl = 'https://chatgpt.com/backend-api/conversation';
+export const createShareUrl = 'https://chatgpt.com/backend-api/share';
+
 export const cookieName = '__Secure-next-auth.session-token';
--- pages/content-ui/src/App.tsx
+++ pages/content-ui/src/App.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@extension/ui/components/button';
 import { useEffect } from 'react';
 import { tokenStorage } from '@extension/storage';
-import { conversationUrl } from '@extension/shared';
+import { conversationUrl, createShareUrl } from '@extension/shared';
 
 function getConversationIdByURL() {
   const url = window.location.href;
@@ -9,21 +9,62 @@ function getConversationIdByURL() {
   return url.split('/c/')?.[1];
 }
 
-async function getCurrentNodeId(conversationId: string, token: string) {
+function createHeader(token: string) {
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+  myHeaders.append('Authorization', token);
+
+  return myHeaders;
+}
+
+async function getCurrentNodeId(conversationId: string, header: Headers) {
   try {
     const response = await fetch(`${conversationUrl}/${conversationId}`, {
       method: 'GET', // or "POST", "PUT", etc. depending on your use case
-      headers: {
-        Authorization: `${token}`, // Adding the Bearer token
-        'Content-Type': 'application/json', // If you're sending JSON data
-      },
+      headers: header,
     });
     const data = await response.json();
 
-    return data?.current_node;
+    return data?.current_node as string | undefined;
   } catch (error) {
     console.error('Error making the request:', error);
     return null;
+  }
+}
+
+async function createShareURL(conversationId: string, currentNodeId: string, header: Headers) {
+  try {
+    const params = { conversation_id: conversationId, current_node_id: currentNodeId, is_anonymous: true };
+
+    const response = await fetch(`${createShareUrl}/create`, {
+      method: 'POST', // or "POST", "PUT", etc. depending on your use case
+      headers: header,
+      body: JSON.stringify(params),
+    });
+    const data = await response.json();
+
+    return { shareId: data?.share_id, shareUrl: data?.share_url };
+  } catch (error) {
+    console.error('Error making the request:', error);
+    return {};
+  }
+}
+
+async function activeShareURL(shareId: string, header: Headers) {
+  try {
+    const params = { is_public: true, is_visible: true, is_anonymous: true };
+
+    const response = await fetch(`${createShareUrl}/${shareId}`, {
+      method: 'PATCH', // or "POST", "PUT", etc. depending on your use case
+      headers: header,
+      body: JSON.stringify(params),
+    });
+    const data = await response.json();
+
+    return Boolean(data);
+  } catch (error) {
+    console.error('Error making the request:', error);
+    return false;
   }
 }
 
@@ -32,11 +73,17 @@ export default function App() {
     const conversationId = getConversationIdByURL();
     const bearerToken = await tokenStorage.get();
 
-    conversationId && bearerToken.token
-      ? getCurrentNodeId(conversationId, bearerToken.token).then(value => {
-          alert(value);
-        })
-      : alert('Token or conversationId not found!');
+    if (conversationId && bearerToken?.token) {
+      const header = createHeader(bearerToken.token);
+      const currentNodeId = await getCurrentNodeId(conversationId, header);
+      const shareData = currentNodeId ? await createShareURL(conversationId, currentNodeId, header) : {};
+
+      const isActivatedSuccess = shareData.shareId ? await activeShareURL(shareData.shareId, header) : false;
+
+      alert(isActivatedSuccess + ' ' + shareData.shareUrl);
+    } else {
+      alert('Token or conversationId not found!');
+    }
   };
 
   useEffect(() => {

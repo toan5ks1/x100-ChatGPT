2b6a261ada509df1ae1fc0ee97f4787369a5d760
toan5ks1 <vipteamvnclass@gmail.com>
2024-10-20 14:57:35 +0700

    chore

--- pages/content-ui/src/App.tsx
+++ pages/content-ui/src/App.tsx
@@ -79,18 +79,13 @@ export default function App() {
   }, []);
 
   return (
-    <>
+    <div>
       <DialogSharedURL
         isOpen={isOpenSharedModal}
         setIsOpen={setIsOpenSharedModal}
         url={sharedData.shareUrl}
         onContinue={() => handleRedirect(sharedData.shareUrl)}
       />
-      {/* <div className="flex items-center justify-between gap-2 rounded bg-blue-100 px-2 py-1">
-        <div className="flex gap-1 text-blue-500">
-          Edit <strong className="text-blue-700">pages/content-ui/src/app.tsx</strong> and save to reload.
-        </div>
-      </div> */}
-    </>
+    </div>
   );
 }
